21843dfd139d0994edd0ee46ce29ad0cba90436d
MUKINH4 <samuelmaragato3@gmail.com>
2026-02-13 16:55:09 -0300

    Exercício 02

---
+++ Exercicios-Unidade-01/Ex02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex02 {
+    //Escreva um programa que leia um número e determine se ele é par ou ímpar. Imprima o resultado na tela
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("Número Par!");
+        } else {
+            System.out.println("Número Ímpar!");
+        }
+
+        scanner.close();
+    }
+}

afc7ed1a2e8b333049b4a6cbb5c0333a94fde362
MUKINH4 <samuelmaragato3@gmail.com>
2026-02-13 16:52:47 -0300

    Exercício 01

---
+++ Exercicios-Unidade-01/Ex01.java
@@ -0,0 +1,14 @@
+public class Ex01 {
+    //Crie um programa que declare variáveis para armazenar um número inteiro, um número de ponto flutuante e uma string. Atribua valores a essas variáveis e exiba-as na tela.
+    public static void main(String[] args ) {
+        int numeroInteiro = 21;
+        float numeroFlutuante = 2.2f;
+        String texto = "Isso eh uma string";
+    
+        System.out.println("Inteiro: " + numeroInteiro);
+        System.out.println("Flutuante: " + numeroFlutuante);
+        System.out.println("String: " + texto);
+        
+    }
+
+}
